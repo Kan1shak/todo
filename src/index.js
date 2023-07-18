29b02f2a6962fc0e16e2c6854a33bb2ddcb58153
Kan1shak <112748208+Kan1shak@users.noreply.github.com>
2023-07-18 21:14:54 +0530

    Allow ScreenController to modify dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-function Projects(){
+function ProjectHolder(){
     const projects = [];
     const getProjects = () => projects;
     const addProject = (project) => {
@@ -12,8 +12,6 @@ function Projects(){
     }
     return {getProjects, addProject, removeProject}
 };
-
-
 function Project(name){
     this.name = name;
     this.tasks = [];
@@ -37,22 +35,34 @@ function Task(title,priority,description,status=false){
 }
 
 const DisplayController = (() => {
-    const projects = Projects();
+    const ProjectsList = ProjectHolder();
     const projectContainer = document.querySelector('.project-container');
     const taskContainer = document.querySelector('.task-container');
     const projectAddButton = document.getElementById('add-proj');
     const taskAddButton = document.getElementById('add-task');
+    var selectedProj = 0;
+    const selectProj = (e) => {
+        document.querySelectorAll('.proj-title').forEach(proj => {
+            proj.classList.remove('selected');
+        });
+        e.target.classList.add('selected');
+        selectedProj = e.target.dataset.index;
+        updateTasks(ProjectsList.getProjects()[selectedProj]);
+    }
     const updateProject = () => {
         projectContainer.textContent = '';
-        const currentProjects = projects.getProjects();
-        currentProjects.forEach(project => {
+        const currentProjects = ProjectsList.getProjects();
+        currentProjects.forEach((project,index) => {
             const projTitle = document.createElement('h4');
             projTitle.classList.add('proj-title');
             projTitle.textContent = project.name;
+            projTitle.addEventListener('click', selectProj);
+            projTitle.dataset.index = index;
             projectContainer.appendChild(projTitle);
+            selectedProj = index;
+            updateTasks(ProjectsList.getProjects()[selectedProj]);
         });
     }
-
     const updateTasks = (project) => {
         taskContainer.textContent = '';
         const currentTasks = project.getTasks();
@@ -60,9 +70,21 @@ const DisplayController = (() => {
             const taskTitle = document.createElement('h4');
             taskTitle.classList.add('task-title');
             taskTitle.textContent = task.title;
+            const taskDesc = document.createElement('p');
+            taskDesc.textContent =  task.description;
             taskContainer.appendChild(taskTitle);
+            taskContainer.appendChild(taskDesc);
         }); 
     }
-
+    const buttonHandler = (() => {
+        projectAddButton.addEventListener('click', () =>{
+            ProjectsList.addProject(new Project("Some title!"));
+            updateProject();
+        });
+        taskAddButton.addEventListener('click', () => {
+            ProjectsList.getProjects()[selectedProj].addTask(new Task('damn', 'kinda high', 'nice desc.'));
+            updateTasks(ProjectsList.getProjects()[selectedProj]);
+        });
+    })();
     updateProject();
 })();
