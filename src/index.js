ee03056e1b2cc3f1f3bf2bc6f67f2e6440395a6d
Kan1shak <112748208+Kan1shak@users.noreply.github.com>
2023-07-25 03:43:27 +0530

    Add overlay dialogue to add a task and change how task is displayed

--- src/index.js
+++ src/index.js
@@ -41,6 +41,7 @@ const DisplayController = (() => {
     const projectAddButton = document.getElementById('add-proj');
     const taskAddButton = document.getElementById('add-task');
     const projDialogue = document.querySelector('.add-proj-content');
+    const taskDialogue = document.querySelector('.add-task-content');
     const overlay = document.querySelector('#overlay');
     let selectedProj = -1;
     const selectProj = (e) => {
@@ -70,13 +71,67 @@ const DisplayController = (() => {
         taskContainer.textContent = '';
         const currentTasks = project.getTasks();
         currentTasks.forEach(task => {
+            //creating task item
+            const taskItem = document.createElement('div');
+            taskItem.classList.add('task-item');
+            taskItem.dataset.index = currentTasks.indexOf(task);
+            //creating task title
             const taskTitle = document.createElement('h4');
             taskTitle.classList.add('task-title');
             taskTitle.textContent = task.title;
+            //creating task description
             const taskDesc = document.createElement('p');
             taskDesc.textContent =  task.description;
-            taskContainer.appendChild(taskTitle);
-            taskContainer.appendChild(taskDesc);
+            taskDesc.classList.add('task-desc');
+            taskItem.addEventListener('click', (e) => {
+                if (!e.target.classList.contains('task-status')){
+                    taskDesc.classList.toggle('expanded');
+                }
+            });
+            //creating task status checkbox
+            const taskStatus = document.createElement('input');
+            taskStatus.type = 'checkbox';
+            taskStatus.classList.add('task-status');
+            if (task.status){
+                taskStatus.checked = true;
+                taskStatus.classList.add('done');
+            }
+            else {
+                taskStatus.checked = false;
+                taskStatus.classList.add('not-done');
+            }
+            //adding task event listener to switch status
+            taskStatus.addEventListener('click', (e) => {
+                task.status = !task.status;
+                taskTitle.classList.toggle('done');
+                taskDesc.classList.toggle('done');
+                taskItem.classList.toggle('done');
+            });
+            const checkStatus = () => {
+                if (task.status){
+                    taskStatus.checked = true;
+                    taskTitle.classList.add('done');
+                    taskDesc.classList.add('done');
+                    taskItem.classList.add('done');
+                }
+                else {
+                    taskStatus.checked = false;
+                    taskTitle.classList.remove('done');
+                    taskDesc.classList.remove('done');
+                    taskItem.classList.remove('done');
+                }
+            }
+            //creating task priority
+            // const taskPriority = document.createElement('span');
+            // taskPriority.classList.add('task-priority');
+            // taskPriority.textContent = task.priority;
+            taskItem.classList.add(task.priority.toLowerCase());
+            //adding all to task item
+            checkStatus();
+            taskItem.appendChild(taskTitle);
+            taskItem.appendChild(taskDesc);
+            taskItem.appendChild(taskStatus);
+            taskContainer.append(taskItem);
         }); 
     }
     const openProjDialogue = () => {
@@ -89,21 +144,24 @@ const DisplayController = (() => {
         overlay.classList.remove('active');
         projTitle.value = '';
     }
+    const openTaskDialogue = () => {
+        taskDialogue.classList.add('form-selected');
+        overlay.classList.add('active');
+    }
+    const closeTaskDialogue = () => {
+        const taskTitle = document.querySelector('#task-name');
+        const taskPriority = document.querySelector('#task-priority');
+        const taskDesc = document.querySelector('#task-description');
+        taskDialogue.classList.remove('form-selected');
+        overlay.classList.remove('active');
+        taskTitle.value = '';
+        taskPriority.value = 'Medium';
+        taskDesc.value = '';
+    }
     const buttonHandler = (() => {
         projectAddButton.addEventListener('click', () =>{
             openProjDialogue();
         });
-        taskAddButton.addEventListener('click', () => {
-            if (selectedProj != -1){
-                ProjectsList.getProjects()[selectedProj].addTask(new Task('damn', 'kinda high', 'nice desc.'));
-                updateTasks(ProjectsList.getProjects()[selectedProj]);
-            }
-            else {
-                deplyToast('You need to make a project first!','error');
-            }
-        });
-
-
         //event listner for proj form
         const projForm = document.getElementById('proj-form');
         projForm.addEventListener('submit', (e)=>{
@@ -123,8 +181,34 @@ const DisplayController = (() => {
         document.body.addEventListener('click', (e)=>{
             if(e.target.classList.contains('overlay')){
                 closeProjDialogue();
+                closeTaskDialogue();
             }
         });
+
+        taskAddButton.addEventListener('click', () => {
+            if (selectedProj != -1){
+                // ProjectsList.getProjects()[selectedProj].addTask(new Task('damn', 'kinda high', 'nice desc.'));
+                // updateTasks(ProjectsList.getProjects()[selectedProj]);
+                openTaskDialogue();
+            }
+            else {
+                deplyToast('You need to make a project first!','error');
+            }
+        });
+        //event listener for task form
+        const taskForm =  document.getElementById('task-form');
+        taskForm.addEventListener('submit', (e)=> {
+            e.preventDefault();
+            const taskTitle = document.querySelector('#task-name').value;
+            const taskPriority = document.querySelector('#task-priority').value;
+            const taskDesc = document.querySelector('#task-description').value;
+            ProjectsList.getProjects()[selectedProj].addTask(new Task(taskTitle, taskPriority, taskDesc));
+            updateTasks(ProjectsList.getProjects()[selectedProj]);
+            closeTaskDialogue();
+        })
+        //event listner for closing task form
+        const closeTaskButton = document.getElementById('close-task');
+        closeTaskButton.addEventListener('click', () => closeTaskDialogue());
     })();
     updateProject();
 })();
