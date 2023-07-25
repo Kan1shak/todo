b35332e235d63bb509513a9b5251f7f698a40702
Kan1shak <112748208+Kan1shak@users.noreply.github.com>
2023-07-25 11:04:02 +0530

    Add folders to sort tasks between different projects based on their type

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import './style.css'
 import {deplyToast} from './toast.js';
 function ProjectHolder(){
     const projects = [];
+    const getFolders = () => {
+        const General = [];
+        const HighPriority = [];
+        const Completed = [];
+        getProjects().forEach(project => {
+            project.getTasks().forEach(task => {
+                if (task.priority === 'High' && !task.status){
+                    HighPriority.push(task);
+                }
+                if (task.status){
+                    Completed.push(task);
+                } else {
+                    General.push(task);
+                }
+            });
+        });
+        return {General,"High Priority":HighPriority,Completed};
+    }
     const getProjects = () => projects;
     const addProject = (project) => {
         projects.push(project);
@@ -10,10 +28,11 @@ function ProjectHolder(){
         const index = projects.indexOf(project);
         projects.splice(index,1);
     }
-    return {getProjects, addProject, removeProject}
+    return {getProjects, addProject, removeProject,getFolders}
 };
-function Project(name){
+function Project(name, category='General'){
     this.name = name;
+    this.category = category;
     this.tasks = [];
     this.addTask = (task) => {
         this.tasks.push(task);
@@ -45,13 +64,17 @@ const DisplayController = (() => {
     const overlay = document.querySelector('#overlay');
     let selectedProj = -1;
     const selectProj = (e) => {
-        document.querySelectorAll('.proj-title').forEach(proj => {
+        document.querySelectorAll('.selected').forEach(proj => {
             proj.classList.remove('selected');
         });
         e.target.classList.add('selected');
         selectedProj = e.target.dataset.index;
-        updateTasks(ProjectsList.getProjects()[selectedProj]);
-        
+        if (e.target.classList.contains('folder-title')){
+            updateTasks(ProjectsList.getFolders()[e.target.textContent],'folder');
+        }
+        else {
+            updateTasks(ProjectsList.getProjects()[selectedProj],'project');
+        }
     }
     const updateProject = () => {
         projectContainer.textContent = '';
@@ -64,13 +87,22 @@ const DisplayController = (() => {
             projTitle.dataset.index = index;
             projectContainer.appendChild(projTitle);
             selectedProj = index;
-            updateTasks(ProjectsList.getProjects()[selectedProj]);
+            updateTasks(ProjectsList.getProjects()[selectedProj],'project');
         });
     }
-    const updateTasks = (project) => {
+    const updateTasks = (project,where) => {
         taskContainer.textContent = '';
-        const currentTasks = project.getTasks();
-        currentTasks.forEach(task => {
+            let currentTasks;
+            if (where === 'folder'){
+                currentTasks = project;
+                if (currentTasks[0] === undefined) {
+                    deplyToast('No tasks in this folder!','info');
+                }
+            }
+            if (where === 'project'){
+                currentTasks = project.getTasks();
+            }
+            currentTasks.forEach(task => {
             //creating task item
             const taskItem = document.createElement('div');
             taskItem.classList.add('task-item');
@@ -132,7 +164,8 @@ const DisplayController = (() => {
             taskItem.appendChild(taskDesc);
             taskItem.appendChild(taskStatus);
             taskContainer.append(taskItem);
-        }); 
+        })
+        updateFolders();
     }
     const openProjDialogue = () => {
         projDialogue.classList.add('form-selected');
@@ -168,16 +201,18 @@ const DisplayController = (() => {
             //Preventing page refresh
             e.preventDefault();
             const projTitle = document.querySelector('#proj-name').value;
-            ProjectsList.addProject(new Project(projTitle));
+            const projCategory = document.querySelector('#proj-category').value;
+            ProjectsList.addProject(new Project(projTitle, projCategory === '' ? 'General' : projCategory));
             updateProject();
             closeProjDialogue();
             const lastProject = document.querySelector(`[data-index=\"${selectedProj}\"]`);
             lastProject.classList.add('selected');
+            deplyToast('Project added!','success');
         })
         //event listner for closing proj form
         const closeProjButton = document.getElementById('close-proj');
         closeProjButton.addEventListener('click', () => closeProjDialogue());
-        //event listner for closing proj when click outside form
+        //event listner for closing forms when click outside form
         document.body.addEventListener('click', (e)=>{
             if(e.target.classList.contains('overlay')){
                 closeProjDialogue();
@@ -186,9 +221,7 @@ const DisplayController = (() => {
         });
 
         taskAddButton.addEventListener('click', () => {
-            if (selectedProj != -1){
-                // ProjectsList.getProjects()[selectedProj].addTask(new Task('damn', 'kinda high', 'nice desc.'));
-                // updateTasks(ProjectsList.getProjects()[selectedProj]);
+            if (selectedProj != -1 && selectedProj != undefined){
                 openTaskDialogue();
             }
             else {
@@ -202,13 +235,30 @@ const DisplayController = (() => {
             const taskTitle = document.querySelector('#task-name').value;
             const taskPriority = document.querySelector('#task-priority').value;
             const taskDesc = document.querySelector('#task-description').value;
-            ProjectsList.getProjects()[selectedProj].addTask(new Task(taskTitle, taskPriority, taskDesc));
-            updateTasks(ProjectsList.getProjects()[selectedProj]);
+            ProjectsList.getProjects()[selectedProj].addTask(new Task(taskTitle, taskPriority, taskDesc === '' ? "No description provided." : taskDesc));
+            updateTasks(ProjectsList.getProjects()[selectedProj],'project');
             closeTaskDialogue();
+            deplyToast('Task added!','success');    
         })
         //event listner for closing task form
         const closeTaskButton = document.getElementById('close-task');
         closeTaskButton.addEventListener('click', () => closeTaskDialogue());
     })();
+    const updateFolders = () => {
+        const folders = ProjectsList.getFolders();
+        const folderContainer = document.querySelector('.folder-container');
+        folderContainer.textContent = '';
+        for (const folder in folders) {
+            const folderItem = document.createElement('div');
+            folderItem.classList.add('folder-item');
+            const folderTitle = document.createElement('h4');
+            folderTitle.classList.add('folder-title');
+            folderTitle.textContent = folder;
+            folderTitle.addEventListener('click', selectProj);
+            folderItem.appendChild(folderTitle);
+            folderContainer.appendChild(folderItem);
+        }
+    };
     updateProject();
+    updateFolders();
 })();
